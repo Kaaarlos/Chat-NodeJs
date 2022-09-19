af22417fc4a4d4f2c29da6bc3f590291615c9df0
Carlos Alberto Ceja Zapata <73798412+Kaaarlos@users.noreply.github.com>
2022-09-19 15:06:10 -0500

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 // make a connection
-var socket = io.connect("https://rainbow-pastelito-bea001.netlify.app/");
+var socket = io.connect("https://candid-beijinho-c5eee7.netlify.app/");
 
 var message = document.getElementById("message");
     (handle = document.getElementById("handle")),
